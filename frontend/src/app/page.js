4524c7a8cc68f8699ab956cfd97a414bd740f997
Amit Maurya <ankitamaurya2908@gmail.com>
2026-01-21 17:14:30 +0530

    feat: created hero section

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,28 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
+import Navbar from "@/components/Navbar";
+import React from "react";
+import { LuLogIn } from "react-icons/lu";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
+    <>
+      <Navbar />
+      <div className="bg-[#d9d9d9] rounded-2xl mx-4 mt-4 min-h-[85vh] flex flex-col items-center justify-center text-center px-6">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-[#353535]">
+          Track your job applications.{" "}
+          <span className="text-[#3c6e71]">Smarter.</span>
+        </h1>
+
+        <p className="mt-6 max-w-5xl text-base sm:text-lg md:text-xl lg:text-2xl text-[#284b63] font-light leading-relaxed">
+          Keep all your job applications, resumes, and progress in one place
+          with ApplyWise. Focus on landing interviews, not managing
+          spreadsheets.
+        </p>
+
+        <button className="mt-8 bg-[#3c6e71] text-base sm:text-lg md:text-xl font-medium text-white px-8 sm:px-10 md:px-12 py-3 md:py-4 rounded-full flex items-center gap-3 hover:bg-[#2f5d60] transition">
+          <LuLogIn className="text-xl md:text-2xl" />
+          Sign Up – It&apos;s 100% free!
+        </button>
+      </div>
+    </>
+  );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,103 @@
+// "use client";
+
+// import { useState } from "react";
+// import Link from "next/link";
+// import { HiMenuAlt3, HiX } from "react-icons/hi";
+// import { zain, poppins } from "@/lib/fonts";
+// import { navLinks } from "@/lib/navlink";
+
+// export default function Navbar() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <nav className="w-full bg-white sticky top-0 z-50">
+//       <div className="mx-auto px-6 py-3 flex items-center justify-between">
+//         <Link
+//           href="/"
+//           className={`${zain.className} text-2xl md:text:3xl lg:text-4xl font-bold text-[#3c6e71]`}
+//         >
+//           applywise
+//         </Link>
+
+//         {/* For Desktop */}
+//         <div
+//           className={`${poppins.className} hidden md:flex gap-4 lg:gap-8 md:text-md lg:text-lg font-light text-[#284b63]`}
+//         >
+//           {navLinks.map((link) => (
+//             <Link
+//               key={link.name}
+//               href={link.href}
+//               className="hover:text-[#3c6e71]"
+//             >
+//               {link.name}
+//             </Link>
+//           ))}
+//         </div>
+
+//         <div className="flex items-center gap-4">
+//           <Link
+//             href="/login"
+//             className="hidden lg:inline-block px-4 py-2 text-lg rounded-md border border-[#3c6e71] text-[#284b63] hover:bg-[#3c6e71] hover:text-white"
+//           >
+//             Login
+//           </Link>
+
+//           <Link
+//             href="/signup"
+//             className="hidden md:inline-block px-4 py-2 text-lg rounded-md text-white bg-[#284b63] hover:bg-[#3c6e71]"
+//           >
+//             Sign up
+//           </Link>
+//         </div>
+
+//         {/* For Mobile */}
+//         <button
+//           onClick={() => setIsOpen(!isOpen)}
+//           className="md:hidden text-[#284b63]"
+//         >
+//           {isOpen ? <HiX size={28} /> : <HiMenuAlt3 size={28} />}
+//         </button>
+//       </div>
+
+//       <div
+//         className={`md:hidden transition-all duration-300 ease-in-out ${
+//           isOpen
+//             ? "max-h-[500px] opacity-100"
+//             : "max-h-0 opacity-0 overflow-hidden"
+//         }`}
+//       >
+//         <div className="px-3 pt-2 pb-6">
+//           <div className="bg-[#3c6e71] rounded-2xl shadow-xl p-2 ">
+//             {/* Nav links */}
+//             {navLinks.map((link) => (
+//               <Link
+//                 key={link.name}
+//                 href={link.href}
+//                 onClick={() => setIsOpen(false)}
+//                 className={`${poppins.className} block text-white text-md font-light px-4 py-3 rounded-lg hover:bg-white/10 transition`}
+//               >
+//                 {link.name}
+//               </Link>
+//             ))}
+
+//             <div className="space-y-3 pb-4 mx-3">
+//               <Link
+//                 href="/signup"
+//                 onClick={() => setIsOpen(false)}
+//                 className={`${poppins.className} block text-center bg-white text-[#3c6e71] px-4 py-3 rounded-lg font-semibold`}
+//               >
+//                 Sign up
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+
+
 "use client";
 
 import { useState } from "react";
@@ -10,16 +110,18 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full bg-white sticky top-0 z-50">
+    <nav className="w-full bg-white sticky top-0 z-50 relative">
       <div className="mx-auto px-6 py-3 flex items-center justify-between">
+
+        {/* Brand */}
         <Link
           href="/"
-          className={`${zain.className} text-2xl md:text:3xl lg:text-4xl font-bold text-[#3c6e71]`}
+          className={`${zain.className} text-2xl md:text-3xl lg:text-4xl font-bold text-[#3c6e71]`}
         >
           applywise
         </Link>
 
-        {/* For Desktop */}
+        {/* Desktop nav */}
         <div
           className={`${poppins.className} hidden md:flex gap-4 lg:gap-8 md:text-md lg:text-lg font-light text-[#284b63]`}
         >
@@ -34,6 +136,7 @@ export default function Navbar() {
           ))}
         </div>
 
+        {/* Desktop auth */}
         <div className="flex items-center gap-4">
           <Link
             href="/login"
@@ -48,27 +151,28 @@ export default function Navbar() {
           >
             Sign up
           </Link>
-        </div>
 
-        {/* For Mobile */}
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="md:hidden text-[#284b63]"
-        >
-          {isOpen ? <HiX size={28} /> : <HiMenuAlt3 size={28} />}
-        </button>
+          {/* Mobile toggle */}
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="md:hidden text-[#284b63]"
+          >
+            {isOpen ? <HiX size={28} /> : <HiMenuAlt3 size={28} />}
+          </button>
+        </div>
       </div>
 
+      {/* Mobile Menu (overlay) */}
       <div
-        className={`md:hidden transition-all duration-300 ease-in-out ${
+        className={`md:hidden absolute top-full left-0 w-full transition-all duration-300 ease-in-out ${
           isOpen
-            ? "max-h-[500px] opacity-100"
-            : "max-h-0 opacity-0 overflow-hidden"
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 -translate-y-2 pointer-events-none"
         }`}
       >
-        <div className="px-3 pt-2 pb-6">
-          <div className="bg-[#3c6e71] rounded-2xl shadow-xl p-2 ">
-            {/* Nav links */}
+        <div className="px-4 pt-3 pb-6">
+          <div className="bg-[#3c6e71] rounded-2xl shadow-xl p-4 space-y-2">
+
             {navLinks.map((link) => (
               <Link
                 key={link.name}
@@ -80,7 +184,7 @@ export default function Navbar() {
               </Link>
             ))}
 
-            <div className="space-y-3 pb-4 mx-3">
+            <div className="pt-4 border-t border-white/20">
               <Link
                 href="/signup"
                 onClick={() => setIsOpen(false)}
@@ -89,6 +193,7 @@ export default function Navbar() {
                 Sign up
               </Link>
             </div>
+
           </div>
         </div>
       </div>
